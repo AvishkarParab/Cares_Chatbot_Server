744224cf5826bb6a2f0e1da211e1ef7fe8b92f83
Avishkar Parab <aviparab72733@gmail.com>
2022-10-21 02:34:58 +0530

    Added the course Controller

--- routes/course.js
+++ routes/course.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const {
-    getCourse
+    getCourse,
+    updateCourse,
+    addCourse,
+    deleteCourse
 
 } = require("../controllers/courseController")
 // const {protect} = require("../middleware/authMiddleware");
 
 
 //get all courses
-router.get("/course",getCourse);
+router.get("/",getCourse);
+
+//update a course
+router.put("/update",updateCourse);
+
+//add a course
+router.post("/add",addCourse);
+
+//delete a course
+router.delete("/",deleteCourse);
 
 
 module.exports = router;
