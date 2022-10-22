32b6ca227a2e884f61dee18972acc6b5414d2ba4
Avishkar Parab <aviparab72733@gmail.com>
2022-10-23 02:41:51 +0530

    Added course function

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const course = require("./course");
+const modu = require("./module");
+
 
 
 router.use("/course",course)
 
+router.use("/module",modu)
 
 
 
